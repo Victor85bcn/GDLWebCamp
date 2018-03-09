5be1987e6922f153b9988861633945a1e2b754d0
Víctor Martínez <victor85bcn@gmail.com>
2018-03-09 14:09:19 +0100

    Optimización

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function() {
+      "use strict";
+
+      document.addEventListener('DOMContentLoaded', function(){
+
+          // Campos datos usuario
+          var nombre = document.getElementById('nombre');
+          var apellido = document.getElementById('apellido');
+          var email = document.getElementById('email');
+
+          // Campos pases
+
+          var pase_dia = document.getElementById('pase_dia');
+          var pase_dosdias = document.getElementById('pase_dosdias');
+          var pase_completo = document.getElementById('pase_completo');
+
+          // Botones y divs
+          var calcular = document.getElementById('calcular');
+          var errorDiv = document.getElementById('error');
+          var botonRegistro = document.getElementById('btnRegistro');
+          var resultado = document.getElementById('lista-productos  ');
+
+
+      }); // DOM CONTENT LOADED
+})();
